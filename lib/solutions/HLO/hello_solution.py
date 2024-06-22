289a95b6f059e2f4bbd1e393df09aba6549d9667
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 13:22:58 +0000

    Sat Jun 22 13:22:58 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,6 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+    """Takes a string and returns a string 'Hello, <friend_name>!'"""
+    return f'Hello, {friend_name}!'
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,6 +2,3 @@
 def compute(x: int, y: int) -> int:
     """Sum two integer types together to return an integer"""
     return x + y
-
-
-
