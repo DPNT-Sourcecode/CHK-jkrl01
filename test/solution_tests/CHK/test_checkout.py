3abad7cec33a955ecb8977f6f45bfba46d437a55
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 14:49:18 +0000

    Sat Jun 22 14:49:18 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,8 +5,17 @@ class TestCheckout():
     def test_checkout__single_sku(self):
         assert checkout_solution.checkout('A') == 50
 
-    def test_checkout__single_sku_multibuy(self):
+    def test_checkout__single_sku_multibuy_offer(self):
         assert checkout_solution.checkout('3A') == 130
+
+    def test_checkout__single_sku_multibuy_nooffer(self):
+        assert checkout_solution.checkout('2A') == 130
+
+    def test_checkout__single_sku_multibuy_mixed(self):
+        """Multi-buy offer can apply to a subset of quanitities
+        4A -> 3A + A -> 130 + 50 = 180
+        """
+        assert checkout_solution.checkout('4A') == 180
 
     def test_checkout__many_sku(self):
         """Assume Delimiter is comma.
@@ -20,3 +29,4 @@ class TestCheckout():
 
     def test_checkout__illegal_1(self):
         assert checkout_solution.checkout('this_is_illegal') == -1
+
