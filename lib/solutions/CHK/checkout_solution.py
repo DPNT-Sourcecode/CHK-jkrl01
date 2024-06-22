4f42260329b27cbe75e8a6851a563d008cfd94fb
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 14:46:18 +0000

    Sat Jun 22 14:46:18 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,10 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
+    """Takes a string of SKUs delimited by comma (,). Multibuys are prefixed
+    with a number.
+    """
     sku_price_map = {
             'A': 50,
             'B': 30,
@@ -15,6 +18,18 @@ def checkout(skus):
             '2B': 45,
             }
 
+    total_price = 0
+
     sku_list = skus.split(',') # assume comma delimiter
 
-    return sku_price_map[skus]
+    for sku in sku_list:
+        if sku in sku_price_map:
+            total_price += sku_price_map[sku]
+            continue
+        if sku in sku_multibuy_map:
+            total_price += sku_multibuy_map[sku]
+            continue
+        return -1 # sku not present in any maps
+
+    return total_price
+
