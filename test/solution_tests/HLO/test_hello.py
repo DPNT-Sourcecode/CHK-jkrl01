9b642acf50428515fa2032d77c465a6b49bb80e2
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 13:47:46 +0000

    Sat Jun 22 13:47:46 UTC 2024

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -6,9 +6,16 @@ class TestHello():
         assert hello_solution.hello('danny') == 'Hello, danny!'
 
     def test_hello__zero_length(self):
+        """Function shall except zero-length names
+
+        This is needs to be confirmed with Client Specification.
+        """
         assert hello_solution.hello('') == 'Hello, !'
 
     def test_hello__type_coercion(self):
+        """This is a type error, but demenstrates type coercion should
+        untype-checked code is run.
+        """
         assert hello_solution.hello(5) == 'Hello, 5!'
 
     def test_hello__fullname(self):
@@ -19,8 +26,3 @@ class TestHello():
 
     def test_hello__clientspec_2(self):
         assert hello_solution.hello("Mr. X") == 'Hello, Mr. X!'
-
-
-
-
-
