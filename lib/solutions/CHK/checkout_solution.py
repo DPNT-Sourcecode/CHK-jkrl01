cecb5d8ea1ec3c1c63616efff9d34d3894e19a6d
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 14:34:18 +0000

    Sat Jun 22 14:34:18 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    sku_price_map = {
+            'A': 50,
+            'B': 30,
+            'C': 20,
+            'D': 15,
+
+            }
     raise NotImplementedError()
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,4 +5,10 @@ class TestCheckout():
     def test_checkout__base(self):
         assert checkout_solution.checkout('A') == 50
 
+    def test_checkout__base_multibuy(self):
+        assert checkout_solution.checkout('3A') == 130
+
+    def test_checkout__illegal_1(self):
+        assert checkout_solution.checkout('this_is_illegal') == -1
+
 
