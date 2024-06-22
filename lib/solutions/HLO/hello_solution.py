039e603cab43bf546e3aafdd1ada5a7941f9a35f
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 13:37:28 +0000

    Sat Jun 22 13:37:28 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -5,8 +5,9 @@
 def hello(friend_name: str) -> str:
     """Takes a string and returns a string 'Hello, <friend_name>!'
 
-    Name is not capitalised, and returned as-is
+    Name is not capitalised, and returned as-is.
 
     """
     return f'Hello, {friend_name}!'
+
 
