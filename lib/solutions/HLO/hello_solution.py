c76dc3f0b44c5152708cadc8e6f0774ddd451a0d
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 13:40:28 +0000

    Sat Jun 22 13:40:28 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,11 +3,12 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
-    """Takes a string and returns a string 'Hello, <friend_name>!'
+    """Takes a string and returns a string 'Hello, World!'
 
-    Name is not capitalised, and returned as-is.
+    Name argument is ignored.
 
     """
-    return f'Hello, {friend_name}!'
+    return f'Hello, World!'
+
 
 
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,12 +3,13 @@ from lib.solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello(self):
-        assert hello_solution.hello('danny') == 'Hello, danny!'
+        assert hello_solution.hello('danny') == 'Hello, World!'
 
     def test_hello__zero_length(self):
-        assert hello_solution.hello('') == 'Hello, !'
+        assert hello_solution.hello('') == 'Hello, World!'
 
     def test_hello__fullname(self):
-        assert hello_solution.hello('Danny Stewart') == 'Hello, Danny Stewart!'
+        assert hello_solution.hello('Danny Stewart') == 'Hello, World!'
+
 
 
