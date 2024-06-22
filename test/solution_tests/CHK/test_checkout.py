198ff496c87159c93d686f2f16cbaff7f37a9099
sbx_user1051 <sbx_user1051@169.254.117.117>
2024-06-22 14:37:18 +0000

    Sat Jun 22 14:37:18 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,13 +2,24 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout():
-    def test_checkout__base(self):
+    def test_checkout__single_sku(self):
         assert checkout_solution.checkout('A') == 50
 
-    def test_checkout__base_multibuy(self):
+    def test_checkout__single_sku_multibuy(self):
         assert checkout_solution.checkout('3A') == 130
+
+    def test_checkout__many_sku(self):
+        """Assume Delimiter is comma.
+
+        Not defined in client specification"""
+        assert checkout_solution.checkout('A,B') == 80
+
+    def test_checkout__many_sku_multibuy(self):
+        """Assume Delimiter is comma"""
+        assert checkout_solution.checkout('3A,B') == 160
 
     def test_checkout__illegal_1(self):
         assert checkout_solution.checkout('this_is_illegal') == -1
+
 
 
