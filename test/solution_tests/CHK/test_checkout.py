59ef62dec5c7be681cead03e8f62c1073a2957cf
sbx_user1051 <sbx_user1051@169.254.35.141>
2024-06-23 08:17:19 +0000

    Sun Jun 23 08:17:19 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -19,6 +19,18 @@ class TestCheckout():
         """
         assert checkout_solution.checkout('AAAA') == 180
 
+    def test_checkout__single_sku_multibuy_1(self):
+        """Multi-buy offer can apply to a subset of quanitities
+        AAAAA -> 200 = 200
+        """
+        assert checkout_solution.checkout('AAAAA') == 200
+
+    def test_checkout__single_sku_multibuy_2(self):
+        """Multi-buy offer can apply to a subset of quanitities
+        AAAAAAAAA -> AAAAA + AAA + A -> 200 + 130 + 50 = 380
+        """
+        assert checkout_solution.checkout('AAAAAAAAA') == 380
+
     def test_checkout__single_sku_multibuy_double_digit_mixed(self):
         """Multi-buy offer can apply to a subset of quanitities
         10A -> 3*AAA + A -> 390 + 50 = 440
@@ -54,5 +66,6 @@ class TestCheckout():
 
     def test_checkout__client_spec_4(self):
         assert checkout_solution.checkout('ABCDABCD') == 215
+
 
 
