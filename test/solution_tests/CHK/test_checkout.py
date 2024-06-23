05770a3eab31874b6e07f1eb0d441b02100930cc
sbx_user1051 <sbx_user1051@169.254.35.141>
2024-06-23 07:35:40 +0000

    Sun Jun 23 07:35:40 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,10 @@
 from lib.solutions.CHK import checkout_solution
 
 
+ # - {"method":"checkout","params":[""],"id":"CHK_R1_002"}, expected: 0, got: -1
+ # - {"method":"checkout","params":["ABCD"],"id":"CHK_R1_011"}, expected: 115, got: -1
+ # - {"method":"checkout","params":["AA"],"id":"CHK_R1_013"}, expected: 100, got: -1
+
 class TestCheckout():
     def test_checkout__single_sku(self):
         assert checkout_solution.checkout('A') == 50
@@ -50,17 +54,18 @@ class TestCheckout():
     def test_checkout__illegal_2(self):
         assert checkout_solution.checkout('3A,5this_is_illegal') == -1
 
-    def test_sku_split__no_quantity(self):
-        assert checkout_solution.sku_split('A') == ('A', 1)
+    # def test_sku_split__no_quantity(self):
+    #     assert checkout_solution.sku_split('A') == ('A', 1)
 
-    def test_sku_split__single_digit_quantity(self):
-        assert checkout_solution.sku_split('2A') == ('A', 2)
+    # def test_sku_split__single_digit_quantity(self):
+    #     assert checkout_solution.sku_split('2A') == ('A', 2)
 
-    def test_sku_split__double_digit_quantity(self):
-        assert checkout_solution.sku_split('10A') == ('A', 10)
+    # def test_sku_split__double_digit_quantity(self):
+    #     assert checkout_solution.sku_split('10A') == ('A', 10)
 
-    def test_sku_split__triple_digit_quantity(self):
-        assert checkout_solution.sku_split('100A') == ('A', 100)
+    # def test_sku_split__triple_digit_quantity(self):
+    #     assert checkout_solution.sku_split('100A') == ('A', 100)
 
-    def test_sku_split__multichar_sku(self):
-        assert checkout_solution.sku_split('100ABDF') == ('ABDF', 100)
+    # def test_sku_split__multichar_sku(self):
+    #     assert checkout_solution.sku_split('100ABDF') == ('ABDF', 100)
+
