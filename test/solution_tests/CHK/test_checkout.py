8c5105525408560156916183b7d16c8829edbdcd
BhawickJain <53153810+BhawickJain@users.noreply.github.com>
2024-06-23 19:05:48 +0100

    chk_r5: Fix 2K multibuy discount price
    Reflects the new price table which was previously missed.

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -178,3 +178,13 @@ class TestCheckout():
         applied
         """
         assert checkout_solution.checkout('SRTRAARAXYZQ') == 362
+
+    def test_checkout__client_spec_5(self):
+        assert checkout_solution.checkout('KK') == 120
+
+    def test_checkout__client_spec_6(self):
+        assert checkout_solution.checkout('KKK') == 190
+
+    def test_checkout__client_spec_7(self):
+        assert checkout_solution.checkout('KKKK') == 240
+
